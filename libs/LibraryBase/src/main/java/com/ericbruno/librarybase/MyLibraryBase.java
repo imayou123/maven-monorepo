07fbb5aa7aef655512058eee3346c0f02b5a1260
Alexandre Gigleux <alexandre.gigleux@gmail.com>
2024-04-11 15:26:14 +0200

    Delete libs directory

--- libs/LibraryBase/src/main/java/com/ericbruno/librarybase/MyLibraryBase.java
+++
@@ -1,9 +0,0 @@
-package com.ericbruno.librarybase;
-
-/**
- * @author ebruno
- */
-public interface MyLibraryBase {
-    public int add(int val1, int val2);
-    public int subtract(int val1, int val2);
-}
